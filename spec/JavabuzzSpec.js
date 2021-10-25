21c9b201cd5f7bf5b47547a4674578b5aac278d1
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-10-25 18:22:33 +0100

    Added tests for isDivisibleByFive and isDivisibleByFifteen

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -10,6 +10,16 @@ describe('Javabuzz', function() {
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     }); 
 
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 15', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
   });
 
   describe('knows when a number is NOT', function() {
@@ -17,6 +27,16 @@ describe('Javabuzz', function() {
     it('divisible by 3', function() {
       javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by 15', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
 
   });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -2,4 +2,12 @@ class Javabuzz {
   isDivisibleByThree(number) {
     return (number % 3 === 0);
   }
+
+  isDivisibleByFive(number) {
+    return (number % 5 === 0);
+  }
+
+  isDivisibleByFifteen(number) {
+    return (number % 15 === 0);
+  }
 }
